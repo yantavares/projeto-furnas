e92fa0eb8952a61e92741fe0dc18f5a14868a066
yantavares <yantdo1@gmail.com>
2024-01-29 17:23:17 -0300

    Add interactive line and multiple line chart generation using Plotly

--- scripts/charts.py
+++ scripts/charts.py
@@ -9,6 +9,17 @@ class GeradorGrafico:
         self.nome_arquivo = nome_arquivo
 
     def grafico_interativo_linha(self, x, y, nome_eixo_x, nome_eixo_y, titulo="", nome_legenda=""):
+        """
+        Cria um gráfico de linha interativo utilizando Plotly.
+
+        :param x: Vetor com os valores do eixo x.
+        :param y: Vetor com os valores do eixo y.
+        :param nome_eixo_x: Nome para ser exibido no eixo x do gráfico.
+        :param nome_eixo_y: Nome para ser exibido no eixo y do gráfico.
+        :param titulo: Título opcional do gráfico.
+        :param nome_legenda: Nome opcional para a legenda da linha.
+        :return: Objeto de figura Plotly contendo o gráfico de linha.
+        """
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=nome_legenda))
 
@@ -23,6 +34,17 @@ class GeradorGrafico:
         self.fig = fig
 
     def grafico_interativo_multiplas_linhas(self, x, y, nome_eixo_x, nome_eixo_y, titulo="", nome_legenda=[]):
+        """
+        Cria um gráfico interativo com múltiplas linhas utilizando Plotly.
+
+        :param x: Vetor com os valores do eixo x, comum a todas as linhas.
+        :param y: Matriz com os valores do eixo y para cada linha.
+        :param nome_eixo_x: Nome para ser exibido no eixo x do gráfico.
+        :param nome_eixo_y: Nome para ser exibido no eixo y do gráfico.
+        :param titulo: Título opcional do gráfico.
+        :param nome_legenda: Lista com os nomes das legendas para cada linha.
+        :return: Objeto de figura Plotly contendo o gráfico com múltiplas linhas.
+        """
         fig = go.Figure()
         for i in range(len(y)):
             legenda = nome_legenda[i] if i < len(nome_legenda) else ""
@@ -39,21 +61,44 @@ class GeradorGrafico:
         self.fig = fig
 
     def exportar_para_pdf(self):
+        """
+        Exporta um gráfico para um arquivo PDF.
+
+        :param fig: Objeto de figura Plotly que será exportado.
+        :param nome_arquivo: Nome do arquivo PDF para salvar o gráfico.
+        """
         if not os.path.exists("imagens"):
             os.mkdir("imagens")
         self.fig.write_image(f"imagens/{self.nome_arquivo}")
 
     def botao_exportar_pdf(self):
+        """
+        Cria e exibe um botão no Google Colab para exportar um gráfico como PDF.
+
+        :param fig: Objeto de figura Plotly que será exportado.
+        :param nome_arquivo: Nome do arquivo PDF para salvar o gráfico.
+        """
+
         botao = Button(description="Exportar como PDF")
         botao.on_click(self._ao_clicar_no_botao)
         display(botao)
 
-    def _ao_clicar_no_botao(self, _):
-        try:
-            self.exportar_para_pdf()
-            # files.download(f'imagens/{self.nome_arquivo}')
-        except Exception as e:
-            print(f"Não foi possível baixar o arquivo: {e}")
+    def _criar_handler_botao(self, fig, nome_arquivo):
+        """
+        Cria um handler para um evento de clique de botão que exporta um gráfico para PDF.
+
+        :param fig: Objeto de figura Plotly que será exportado.
+        :param nome_arquivo: Nome do arquivo PDF para salvar o gráfico.
+        :return: Função que será chamada quando o botão for clicado.
+        """
+        def ao_clicar_no_botao(_):
+            self.exportar_para_pdf(self, fig, nome_arquivo)
+            try:
+                pass
+                # files.download(f'imagens/{nome_arquivo}')
+            except Exception as e:
+                print(f"Não foi possível baixar o arquivo: {e}")
+        return ao_clicar_no_botao
 
 
 if __name__ == "__main__":
